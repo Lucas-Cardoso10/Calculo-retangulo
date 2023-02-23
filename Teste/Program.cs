ac4e1d42188bddd4ec9c1e4447ed05d14d62db09
julia <julia@DESKTOP-VANDQKR>
2023-02-23 17:24:24 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using Teste;
+
+Retangulo ret = new Retangulo();
+
+Console.WriteLine("Entre a largura e altura do retângulo: ");
+Console.Write("Altura: ");
+ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Largura: ");
+ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Area " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Perimetro " + ret.perimetro().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Diagonal " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+Console.ReadLine();
+
---
+++ Teste/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Teste
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura; ;
+        }
+        public double perimetro()
+        {
+            return (Largura + Altura) * 2;
+
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+    }
+}
